09f8f09a7024235110e2e9ed8aeca93ab1edef13
yeshedj <157068258+yeshedj@users.noreply.github.com>
2024-09-02 15:39:59 -0500

    made landing page

--- quiz-app/app/page.js
+++ quiz-app/app/page.js
@@ -8,43 +8,135 @@ export default function Home() {
     const [name, setName] = useState('');
 
     return (
-        <div className="container mt-5 ml-5">
-            <div className="text-center">
+        <div style={{ 
+                    background: 'linear-gradient(to bottom, #BCA7C9 , #F5F5F5 )', 
+                    minHeight: '100vh', 
+                    padding: '20px' 
+                }}>
+            <div className="container mt-5 ml-5">
+                <div className="text-center">
 
-                <h1 className='text-success mtb-1 '>
-                    GeekForGeeks
+                <h1 className='mtb-1'>
+                    <span style={{ color: 'black' }}>r</span>
+                    <span style={{ color: '#7E38B7' }}>.</span>
+                    <span style={{ color: 'black' }}>care</span>
                 </h1>
-                <h3 className='mb-4'>Quiz App</h3>
-            </div>
+                <br/>
+                <br/>
+                <br/>
 
-            {quizStarted ? (
-                <Quiz name={name} />
-            ) : (
-                <>
-                    <div className="mb-3">
-                        <label htmlFor="nameInput"
-                            className="form-label">
-                            Enter Your Name:
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="nameInput"
-                            value={name}
-                            onChange={(e) =>
-                                setName(e.target.value)}
-                        />
-                    </div>
-                    <button
-                        onClick={() => setQuizStarted(true)}
-                        className="btn btn-primary"
-                        // Disable button if name is empty or whitespace
-                        disabled={!name.trim()}
-                    >
-                        Start Quiz
-                    </button>
-                </>
-            )}
+                    <h3 className='mb-4'>
+                        <strong>Get a personalized <br/>
+                        program </strong><span style={{ color: '#4D4D4D' }}>to manage</span> <br />
+                        <span style={{ color: '#4D4D4D' }}>your binge eating ✨</span>
+                    </h3>
+
+                    <h6>
+                        
+                        <span style={{ color: '#4D4D4D' }}>Select your age to start the quiz.</span>
+                        <br/> <br/>
+                        <span style={{ color: '#A9A9A9' }}>⌛ 3 min quiz</span>
+                    </h6>
+                </div>
+
+                <br/><br/>
+
+                {quizStarted ? (
+                    <Quiz name={name} />
+                ) : (
+                    <>
+                        {/* <div className="mb-3">
+                            <label htmlFor="nameInput"
+                                className="form-label">
+                                Enter Your Name:
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="nameInput"
+                                value={name}
+                                onChange={(e) =>
+                                    setName(e.target.value)}
+                            />
+                        </div> */}
+                        <center><button
+                            onClick={() => setQuizStarted(true)}
+                            className="btn btn-primary btn-large"
+                            style={{ 
+                                backgroundColor: '#7E38B7', 
+                                marginRight: '13px', 
+                                marginBottom: '13px',  
+                                width: '110px', 
+                                height: '90px' , 
+                                border:'none', 
+                                outline:'none',
+                                borderRadius:'15px',
+                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
+                            }}
+
+                        >
+                            13-17
+                        </button>
+
+                        <button
+                            onClick={() => setQuizStarted(true)}
+                            className="btn btn-primary btn-large"
+                            style={{ 
+                                backgroundColor: '#7E38B7', 
+                                marginRight: '13px', 
+                                marginBottom: '13px',  
+                                width: '110px', 
+                                height: '90px', 
+                                border:'none', 
+                                outline:'none',
+                                borderRadius:'15px',
+                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
+                            }}
+
+                        >
+                            18-29
+                        </button>
+                        <br/>
+                        <button
+                            onClick={() => setQuizStarted(true)}
+                            className="btn btn-primary btn-large"
+                            style={{ 
+                                backgroundColor: '#7E38B7', 
+                                marginRight: '13px', 
+                                marginBottom: '13px', 
+                                width: '110px', 
+                                height: '90px', 
+                                border:'none', 
+                                outline:'none',
+                                borderRadius:'15px',
+                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
+                            }}
+
+                        >
+                            30-49
+                        </button>
+
+                        <button
+                            onClick={() => setQuizStarted(true)}
+                            className="btn btn-primary btn-large"
+                            style={{ 
+                                backgroundColor: '#7E38B7', 
+                                marginRight: '13px', 
+                                marginBottom: '13px', 
+                                width: '110px', 
+                                height: '90px', 
+                                border:'none', 
+                                outline:'none',
+                                borderRadius:'15px',
+                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
+                            }}
+
+                        >
+                            60+
+                        </button></center>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
