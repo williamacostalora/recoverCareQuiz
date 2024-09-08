a8f5e7dda57ee8961eac538875c98fd20347c50a
karlamartinez03 <kaitaely@gmail.com>
2024-09-08 01:04:21 -0500

    Question 1 page js missing bar and arrow

--- quiz-app/app/question-1/page.js
+++ quiz-app/app/question-1/page.js
@@ -1,143 +1,173 @@
-// page.js
+
 'use client'
 import { useState } from "react";
-import Quiz from "./quiz";
 
-export default function Home() {
+export default function Question2() {
     const [quizStarted, setQuizStarted] = useState(false);
     const [name, setName] = useState('');
 
     return (
         <div style={{ 
-                    background: 'linear-gradient(to bottom, #E4C9F4 20%, #F5F5F5 50%)', 
-
+                    background: 'white', 
                     minHeight: '100vh', 
                     padding: '20px' 
                 }}>
             <div className="container mt-5 ml-5">
                 <div className="text-center">
+                    <center>
+                        <small className='mtb-1'>
+                            <span style={{ color: '#7D7D7D' }}>Profile</span>
+                        </small>
+                        <br/>
 
-                <h1 className='mtb-1'>
-                    <span style={{ color: 'black' }}>r</span>
-                    <span style={{ color: '#945DD9' }}>.</span>
-                    <span style={{ color: 'black' }}>care</span>
-                </h1>
-                <br/>
-                <br/>
-                <br/>
+                    <img 
+                        src="/quiz-app/public/assets/q2-bar.jpeg" 
+                        alt="progress bar" 
+                        style={{ width: '200px', height: 'auto' }} 
+                    />
+                    </center>
+
+
+                    <br/>
+                    <br/>
+                    <br/>
+                    <center>
+                        <h3 className='mb-4'>
+                            
+                        <span style={{ color: '#4D4D4D' }}>
+                            Which gender do you identify with? <br/>
+                           
+                        </span>
+                            
+                        </h3>
+                        <br/>
+                        
+                    </center>
+                        
+                    </div>
+                    <br/><br/>
+                    
+
+            
 
                     
-                        <h3 className='mb-4'>
-                            Built by psychologists &  <br/>
-                            binge eating experts <br />
-                        </h3>
 
-                    <small>
-                        
-                        <span style={{ color: '#4D4D4D' }}>Select your age to start the quiz.</span>
-                        <br/> <br/>
-                        <span style={{ color: '#A9A9A9' }}>⌛ 3 min quiz</span>
-                    </small>
-                </div>
+                    {quizStarted ? (
+                        <Quiz name={name} />
+                    ) : (
+                        <>
+                            {/* <div className="mb-3">
+                                <label htmlFor="nameInput"
+                                    className="form-label">
+                                    Enter Your Name:
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="nameInput"
+                                    value={name}
+                                    onChange={(e) =>
+                                        setName(e.target.value)}
+                                />
+                            </div> */}
+                            <center>
+                                <button
+                                    onClick={() => setQuizStarted(true)}
+                                    className="btn btn-primary btn-large"
+                                    style={{ 
+                                        backgroundColor: '#F0F0F0', 
+                                        marginRight: '13px', 
+                                        marginBottom: '13px',  
+                                        width: '300px', 
+                                        height: '50px' , 
+                                        border:'none', 
+                                        outline:'none',
+                                        borderRadius:'15px',
+                                       
+                                    }}
 
-                <br/><br/>
+                                >
+                                    Female
+                                </button>
+                                <br/>
+                                <button
+                                    onClick={() => setQuizStarted(true)}
+                                    className="btn btn-primary btn-large"
+                                    style={{ 
+                                        backgroundColor: '#F0F0F0', 
+                                        marginRight: '13px', 
+                                        marginBottom: '13px',  
+                                        width: '300px', 
+                                        height: '50px' , 
+                                        border:'none', 
+                                        outline:'none',
+                                        borderRadius:'15px',
+                                        
+                                    }}
 
-                {quizStarted ? (
-                    <Quiz name={name} />
-                ) : (
-                    <>
-                        {/* <div className="mb-3">
-                            <label htmlFor="nameInput"
-                                className="form-label">
-                                Enter Your Name:
-                            </label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="nameInput"
-                                value={name}
-                                onChange={(e) =>
-                                    setName(e.target.value)}
-                            />
-                        </div> */}
-                        <center><button
-                            onClick={() => setQuizStarted(true)}
-                            className="btn btn-primary btn-large"
-                            style={{ 
-                                backgroundColor: '#945DD9', 
-                                marginRight: '13px', 
-                                marginBottom: '13px',  
-                                width: '110px', 
-                                height: '90px' , 
-                                border:'none', 
-                                outline:'none',
-                                borderRadius:'15px',
-                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
-                            }}
+                                >
+                                    Male
+                                </button>
+                                <br/>
+                                <button
+                                    onClick={() => setQuizStarted(true)}
+                                    className="btn btn-primary btn-large"
+                                    style={{ 
+                                        backgroundColor: '#F0F0F0', 
+                                        marginRight: '13px', 
+                                        marginBottom: '13px',  
+                                        width: '300px', 
+                                        height: '50px' , 
+                                        border:'none', 
+                                        outline:'none',
+                                        borderRadius:'15px',
+                                        
+                                    }}
 
-                        >
-                            13-17
-                        </button>
-
-                        <button
-                            onClick={() => setQuizStarted(true)}
-                            className="btn btn-primary btn-large"
-                            style={{ 
-                                backgroundColor: '#945DD9', 
-                                marginRight: '13px', 
-                                marginBottom: '13px',  
-                                width: '110px', 
-                                height: '90px', 
-                                border:'none', 
-                                outline:'none',
-                                borderRadius:'15px',
-                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
-                            }}
-
-                        >
-                            18-29
-                        </button>
-                        <br/>
-                        <button
-                            onClick={() => setQuizStarted(true)}
-                            className="btn btn-primary btn-large"
-                            style={{ 
-                                backgroundColor: '#945DD9', 
-                                marginRight: '13px', 
-                                marginBottom: '13px', 
-                                width: '110px', 
-                                height: '90px', 
-                                border:'none', 
-                                outline:'none',
-                                borderRadius:'15px',
-                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
-                            }}
-
-                        >
-                            30-49
-                        </button>
-
-                        <button
-                            onClick={() => setQuizStarted(true)}
-                            className="btn btn-primary btn-large"
-                            style={{ 
-                                backgroundColor: '#945DD9',
-                                marginRight: '13px', 
-                                marginBottom: '13px', 
-                                width: '110px', 
-                                height: '90px', 
-                                border:'none', 
-                                outline:'none',
-                                borderRadius:'15px',
-                                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)'
-                            }}
-
-                        >
-                            50+
-                        </button></center>
-                    </>
-                )}
+                                >
+                                    Non-binary
+                                </button>
+                                <br/>
+                                <button
+                                    onClick={() => setQuizStarted(true)}
+                                    className="btn btn-primary btn-large"
+                                    style={{ 
+                                        backgroundColor: '#F0F0F0', 
+                                        marginRight: '13px', 
+                                        marginBottom: '13px',  
+                                        width: '300px', 
+                                        height: '50px' , 
+                                        border:'none', 
+                                        outline:'none',
+                                        borderRadius:'15px',
+                                        
+                                    }}
+                                >
+                                    Prefer not to answer
+                                </button>
+                                <br/>
+                                {/* <button
+                                    onClick={() => setQuizStarted(true)}
+                                    className="btn btn-primary btn-large"
+                                    style={{ 
+                                        backgroundColor: '#F0F0F0', 
+                                        marginRight: '13px', 
+                                        marginBottom: '13px',  
+                                        width: '300px', 
+                                        height: '50px' , 
+                                        border:'none', 
+                                        outline:'none',
+                                        borderRadius:'15px',
+                                        
+                                    }}
+                                >
+                                    Something else
+                                </button> */}
+                            </center>
+                        </>
+                    )}
             </div>
+            
         </div>
     );
 }
